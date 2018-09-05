66d63f04be8278f0414b86acb6305c7afcb9d547
nyboer <p@nbor.us>
2018-09-05 13:10:50 -0700

    field additions

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ express()
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
   .get('/', function(req, res){
-    //order_id buyer_name buyer_email product_id signature
+    //https://polar-sands-88575.herokuapp.com/?buyer_email={{ order.buyer_email }}&buyer_name={{ order.buyer_name }}&order_id={{ order.id }}&product_id={{ product.id }}&product_name={{ product.name }}
     var order_id = req.query.order_id;
     var product_id = req.query.product_id;
+    var product_name = req.query.product_name;
     var buyer_name = req.query.buyer_name;
     var buyer_email = req.query.buyer_email;
     var signature = req.query.signature;
     console.log('buyer_email: '+buyer_email);
-    console.log('SIG: '+signature);
+    console.log('product_name: '+product_name);
+    console.log('signature: '+signature);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
