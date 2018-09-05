f4289006953e1763ff0baca1aba562286f6fd47b
nyboer <p@nbor.us>
2018-09-05 13:44:10 -0700

    added secrets from heroku env variables

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+//set in heroku https://devcenter.heroku.com/articles/config-vars using https://www.sendowl.com/settings/api_credentials
+const SO_KEY = process.env.SO_KEY;
+const SO_SECRET = process.env.SO_SECRET;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,12 +14,13 @@ express()
     //https://polar-sands-88575.herokuapp.com/?buyer_email={{ order.buyer_email }}&buyer_name={{ order.buyer_name }}&order_id={{ order.id }}&product_id={{ product.id }}&product_name={{ product.name }}
     console.log(req.params);
     console.log('-------');
+    var buyer_email = req.query.buyer_email;
+    var buyer_name = req.query.buyer_name;
     var order_id = req.query.order_id;
     var product_id = req.query.product_id;
     var product_name = req.query.product_name;
-    var buyer_name = req.query.buyer_name;
-    var buyer_email = req.query.buyer_email;
     var signature = req.query.signature;
+    var params_ordered = 'buyer_email='+buyer_email+'&buyer_name'+buyer_name+'&order_id'+order_id+'&product_id'+product_id+'&product_name'+product_name
     console.log('buyer_email: '+buyer_email);
     console.log('product_name: '+product_name);
     console.log('signature: '+signature);
