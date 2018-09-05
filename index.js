6c58da23919854c9898f733de24902ab312352d9
nyboer <p@nbor.us>
2018-09-05 12:53:59 -0700

    fix errors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
   .get('/', function(req, res){
     //order_id buyer_name buyer_email product_id signature
     var order_id = req.query.order_id;
@@ -14,6 +14,6 @@ express()
     var buyer_name = req.query.buyer_name;
     var buyer_email = req.query.buyer_email;
     var signature = req.query.signature;
-    console.log('buyer_email: '+buyer_email));
+    console.log('buyer_email: '+buyer_email);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
