9a4a53784cea470714a0f4c2a97cfd73ad126e21
nyboer <p@nbor.us>
2018-09-05 14:45:46 -0700

    add crypto attempt

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 5000
 //set in heroku https://devcenter.heroku.com/articles/config-vars using https://www.sendowl.com/settings/api_credentials
 const SO_KEY = process.env.SO_KEY;
 const SO_SECRET = process.env.SO_SECRET;
+if(SO_KEY==undefined){
+  console.log('SO_KEY undef');
+}
+if(SO_KEY==undefined){
+  console.log('SO_SECRET undef');
+}
+
+var crypto = require('crypto');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -20,9 +28,18 @@ express()
     var product_id = req.query.product_id;
     var product_name = req.query.product_name;
     var signature = req.query.signature;
-    var params_ordered = 'buyer_email='+buyer_email+'&buyer_name'+buyer_name+'&order_id'+order_id+'&product_id'+product_id+'&product_name'+product_name
+    var params_ordered = 'buyer_email='+buyer_email+'&buyer_name'+buyer_name+'&order_id'+order_id+'&product_id'+product_id+'&product_name'+product_name;
+    var crypto_text = params_ordered+'&secret'+SO_SECRET;
+    var crypto_key = SO_KEY+'&'+SO_SECRET;
+    var crypto_hash = crypto.createHmac('sha1', crypto_key).update(crypto_text).digest('base64');
     console.log('buyer_email: '+buyer_email);
+    console.log('buyer_name: '+buyer_name);
+    console.log('order_id: '+order_id);
+    console.log('product_id: '+product_id);
     console.log('product_name: '+product_name);
     console.log('signature: '+signature);
+    console.log('crypto_hash: '+crypto_hash);
   })
   .listen(PORT, () => console.log(`We're listening on ${ PORT }`))
+
+//localhost:5000/?buyer_email=junk&buyer_name=junk&order_id=junk&product_id=junk&product_name=junk&signature=12345
