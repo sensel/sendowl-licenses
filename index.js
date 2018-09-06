2a09c5a59e97a320281d6f13b5fd1c3147cb988c
nyboer <p@nbor.us>
2018-09-06 13:18:01 -0700

    broke out function for signature

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//require('dotenv').load();
+
+var locallydb = require('locallydb');
+var crypto = require('crypto');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -7,7 +9,7 @@ var SOKEY = process.env.SO_KEY;
 var SOSECRET = process.env.SO_SECRET;
 const ISLOCAL = process.env.LOCAL;
 
-//for testing
+//for testing http://localhost:5000/?order_id=12345&buyer_name=Test+Man&buyer_email=test%40test.com&product_id=123&signature=QpIEZjEmEMZV%2FHYtinoOj5bqAFw%3D
 if(ISLOCAL){
   SOKEY='publicStr';
   SOSECRET='t0ps3cr3t';
@@ -21,35 +23,48 @@ if(!SOSECRET){
   console.log('SO_SECRET '+SOSECRET);
 }
 
-var crypto = require('crypto');
+// load the database (folder) in './db', will be created if doesn't exist
+var db = new locallydb('./db');
+var collection = db.collection('arturia');
+
+
+var calc_sig = function (req,res){
+  //https://polar-sands-88575.herokuapp.com/?buyer_email={{ order.buyer_email }}&buyer_name={{ order.buyer_name }}&order_id={{ order.id }}&product_id={{ product.id }}
+  console.log('----Calculating Signature---');
+  var buyer_email = req.query.buyer_email;
+  var buyer_name = req.query.buyer_name;
+  var order_id = req.query.order_id;
+  var product_id = req.query.product_id;
+  //var product_name = req.query.product_name;
+  var signature = req.query.signature;
+  var params_ordered = 'buyer_email='+buyer_email+'&buyer_name='+buyer_name+'&order_id='+order_id+'&product_id='+product_id; //+'&product_name='+product_name;
+  var crypto_text = params_ordered+'&secret='+SOSECRET;
+  var crypto_key = SOKEY+'&'+SOSECRET;
+  var crypto_hash = crypto.createHmac('sha1', crypto_key).update(crypto_text).digest('base64');
+  for(i in req.query){
+    console.log(i+': '+req.query[i]+'\r');
+  }
+  if(crypto_hash==signature){
+    proc_order();
+  }else{
+    order_invalid();
+  }
+}
+
+function proc_order(){
+  console.log("processing order");
+
+
+}
+
+function order_invalid(){
+  console.log("ORDER INVALID")
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
-  .get('/', function(req, res){
-    //https://polar-sands-88575.herokuapp.com/?buyer_email={{ order.buyer_email }}&buyer_name={{ order.buyer_name }}&order_id={{ order.id }}&product_id={{ product.id }}
-    console.log(req.params);
-    console.log('-------');
-    var buyer_email = req.query.buyer_email;
-    var buyer_name = req.query.buyer_name;
-    var order_id = req.query.order_id;
-    var product_id = req.query.product_id;
-    //var product_name = req.query.product_name;
-    var signature = req.query.signature;
-    var params_ordered = 'buyer_email='+buyer_email+'&buyer_name='+buyer_name+'&order_id='+order_id+'&product_id='+product_id; //+'&product_name='+product_name;
-    //var params_ordered = 'buyer_email=test@test.com&buyer_name=Test Man&order_id=12345&product_id=123';
-    var crypto_text = params_ordered+'&secret='+SOSECRET;
-    var crypto_key = SOKEY+'&'+SOSECRET;
-    var crypto_hash = crypto.createHmac('sha1', crypto_key).update(crypto_text).digest('base64');
-    console.log('buyer_email: '+buyer_email);
-    console.log('buyer_name: '+buyer_name);
-    console.log('order_id: '+order_id);
-    console.log('product_id: '+product_id);
-    console.log('params_ordered: '+params_ordered);
-    //console.log('product_name: '+product_name);
-    console.log('signature: '+signature);
-    console.log('crypto_hash: '+crypto_hash);
-  })
-  .listen(PORT, () => console.log(`We're listening on ${ PORT }`))
+  .get('/', calc_sig)
+  .listen(PORT, () => console.log(`We're listening on ${ PORT }`));
