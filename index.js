bf5c5e6f727d68dd69609fec89a1928fe2c7f1b9
nyboer <p@nbor.us>
2018-09-14 12:05:21 -0700

    guess

--- index.js
+++ index.js
@@ -234,26 +234,27 @@ express()
   .get('/', calc_sig)
   //.post('/shopify/webhook',parseit)
   .post('/shopify/webhook', function (req, res) {
-    console.log('We got an order!')
+    console.log('We got an order!');
     // We'll compare the hmac to our own hash
-    const hmac = req.get('X-Shopify-Hmac-Sha256')
+    const hmac = req.get('X-Shopify-Hmac-Sha256');
     // Use raw-body to get the body (buffer)
-    const body = getRawBody(req)
-    // Create a hash using the body and our key
-    const hash = crypto
-      .createHmac('sha256', SHOPSECRET)
-      .update(body, 'utf8', 'hex')
-      .digest('base64')
-    // Compare our hash to Shopify's hash
-    if (hash === hmac) {
-      // It's a match! All good
-      console.log('Phew, it came from Shopify!');
-      res.sendStatus(200);
-      //parseit
-    } else {
-      // No match! This request didn't originate from Shopify
-      console.log('Danger! Not from Shopify!');
-      res.sendStatus(403);
-    }
+    const body = getRawBody(req, function(req,res){
+      // Create a hash using the body and our key
+      const hash = crypto;
+        .createHmac('sha256', SHOPSECRET)
+        .update(body, 'utf8', 'hex')
+        .digest('base64')
+      // Compare our hash to Shopify's hash
+      if (hash === hmac) {
+        // It's a match! All good
+        console.log('Phew, it came from Shopify!');
+        res.sendStatus(200);
+        //parseit
+      } else {
+        // No match! This request didn't originate from Shopify
+        console.log('Danger! Not from Shopify!');
+        res.sendStatus(403);
+      }
+    });
   })
   .listen(PORT, () => console.log(`We're listening on ${ PORT }`));
