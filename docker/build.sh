25a4370a757115eb920ccb15d5b270953325a471
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2018-09-26 11:56:06 -0700

    add basic docker-compose setup for mongodb and node

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t sensel-software-licenses .
+docker-compose build
--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-export SRC_DIR="$PWD"/../
-export DEST_DIR='/usr/src/app'
-docker run --rm -it -v "$SRC_DIR":"$DEST_DIR" -w "$DEST_DIR" sensel-software-licenses '/bin/bash'
+docker-compose run node bash
